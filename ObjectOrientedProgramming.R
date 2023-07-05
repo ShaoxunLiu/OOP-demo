bae4dc5e1cd05315e1177d1c8ff2d99e31cb964a
ShaoxunLiu <sl4970@columbia.edu>
2023-07-05 09:41:14 -0400

    upload

---
+++ ObjectOrientedProgramming.R
@@ -0,0 +1,194 @@
+####slide 3####
+#input 2 sequences
+DNA <- bio3d::read.fasta(r'(P53DNA.fasta)')
+DNA <- paste(DNA$ali, collapse = '')
+DNA <- substr(DNA, 45*3+1, nchar(DNA))
+DNA <- Biostrings::DNAString(DNA)
+AA <- as.character(Biostrings::translate(DNA))
+DNA <- as.character(DNA)
+AA
+DNA
+
+
+#I want to get the 5th residue
+getResidue <- function(seq, pos){
+  return(substr(seq,pos,pos))
+}
+
+#lets try on the two sequences
+getResidue(AA,5)
+getResidue(DNA,5)
+
+#make the two sequences into two differnt class of objects
+P53DNA <- list(name = 'P53', seq = DNA)
+class(P53DNA) <- 'DNA'
+
+P53AA <- list(name = 'P53', seq = AA)
+class(P53AA) <- 'AA'
+
+class(P53DNA)
+class(P53AA)
+
+#write a generic function
+getResidue <- function(seq, pos) UseMethod("getResidue")
+
+#write the specific methods for the two classes
+getResidue.DNA <- function(seq, pos){
+  codon <- substr(seq$seq, pos*3-2, pos*3)
+  resi <- as.character(Biostrings::GENETIC_CODE[[codon]])
+  out <- list(resi = resi)
+  class(out) <- 'AminoAcid'
+  return(out)
+}
+
+getResidue.AA <- function(seq, pos){
+  resi <- substr(seq$seq, pos, pos)
+  out <- list(resi = resi)
+  class(out) <- 'AminoAcid'
+  return(out)
+}
+
+#see how it works
+getResidue(P53AA,5)
+getResidue(P53DNA,5)
+
+#what if we have multiple sequences in the data set?
+dt <- list(P53DNA,P53AA,P53AA,P53DNA,P53DNA,P53AA,P53DNA)
+unlist(lapply(dt, getResidue, pos = 5))
+
+
+
+####slide 4####
+#get training data of speed and distance
+df <- datasets::cars
+head(df)
+#I want to predict distance according to speed
+speeds <- data.frame(speed = c(10,11,12,13,14,15))
+
+#building linear model
+linear_model <- lm(dist~speed, data = df)
+summary(linear_model)
+
+#building random forest model
+random_forest_model <- randomForest::randomForest(dist~speed, data = df)
+summary(random_forest_model)
+
+#building SVM model
+SVM_model <- e1071::svm(dist~speed, data = df)
+summary(SVM_model)
+
+#predicting with different models
+predict(linear_model, speeds)
+predict(random_forest_model, speeds)
+predict(SVM_model, speeds)
+
+#whats going on with predict()?
+sloop::ftype(predict)
+
+####slide 5####
+#how if we write the AA and DNA classes in S4?
+setClass('DNA',
+         slots = list(name = 'character',
+                      source = 'character',
+                      seq = 'character',
+                      is.p53 = 'logical'))
+
+setClass('AA',
+         slots = list(name = 'character',
+                      source = 'character',
+                      seq = 'character',
+                      is.p53 = 'logical'))
+
+setClass('AminoAcid',
+         slots = list(residueType = 'character'))
+
+setGeneric("getResidue", function(seq, pos){
+  standardGeneric("getResidue")
+})
+
+setMethod("getResidue",
+          c(seq = "DNA", pos = 'numeric'),
+          function(seq, pos){
+            codon <- substr(seq@seq, pos*3-2, pos*3)
+            resi <- as.character(Biostrings::GENETIC_CODE[[codon]])
+            out <- new('AminoAcid', residueType = resi)
+            return(out)
+          })
+
+setMethod("getResidue",
+          c(seq = "AA", pos = 'numeric'),
+          function(seq, pos){
+            resi <- substr(seq@seq, pos, pos)
+            out <- new('AminoAcid', residueType = resi)
+            return(out)
+          })
+
+#see how it works
+P53DNA <- new('DNA', name = 'P53', source = 'Hs', seq = DNA, is.p53 = T)
+P53AA <- new('AA', name = 'P53', source = 'Hs', seq = AA, is.p53 = T)
+getResidue(P53AA,5)
+getResidue(P53DNA,5)
+
+#what if we have multiple sequences in the data set?
+dt <- list(P53DNA,P53AA,P53AA,P53DNA,P53DNA,P53AA,P53DNA)
+unlist(lapply(dt, getResidue, pos = 5))
+
+#what to do if we want a vector of those residues?
+setGeneric("getChar", function(x){
+  standardGeneric("getChar")
+})
+
+setMethod("getChar",
+          c(x = 'AminoAcid'),
+          function(x){
+            return(x@residueType)
+          })
+
+AminoAcids <- lapply(dt, getResidue, pos = 5)
+unlist(lapply(AminoAcids, getChar))
+
+#classes can inherit from other classes
+setClass("p53_DNA",
+         slots = list(mutated = 'logical',
+                      mut_pos = 'numeric'),
+         contains = "DNA")
+
+setClass("p53_AA",
+         slots = list(mutated = 'logical',
+                      mut_pos = 'numeric'),
+         contains = "AA")
+
+#why is this important? lets try to mutate P53
+setClass('AminoAcid',
+         slots = list(residueType = 'character'))
+
+setClass("Base",
+         slots = list(baseType = 'character'))
+
+
+setGeneric("mutateP53", function(seq, pos, mutTo){
+  standardGeneric("mutateP53")
+})
+
+setMethod("mutateP53",
+          c(seq = "DNA", pos = 'numeric', mutTo = 'Base'),
+          function(seq, pos, mutTo){
+            substr(seq@seq, pos, pos) <- mutTo@baseType
+            return(seq)
+          })
+
+setMethod("mutateP53",
+          c(seq = "AA", pos = 'numeric', mutTo = 'AminoAcid'),
+          function(seq, pos, mutTo){
+            substr(seq@seq, pos, pos) <- mutTo@residueType
+            return(seq)
+          })
+
+b <- new('Base', baseType = 'A')
+a <- new('AminoAcid', residueType = 'K')
+
+newP53DNA <- mutateP53(P53DNA, 13, b)
+getResidue(newP53DNA,5)
+
+newP53AA <- mutateP53(P53AA, 5, a)
+getResidue(newP53AA,5)
